234431c433251974e3d585733485a42f6111fc2f
Prashanth Balakrishnan <prashbala13@gmail.com>
2023-10-29 18:19:48 -0400

    added styles for desktop
    not responsive yet

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import TipCalculator from './components/TipCalculator'
 const App = () => {
   return (
     <main>
+      <div className="logo">
+        <p>SPLI</p>
+        <p>TTER</p>
+      </div>
       <TipCalculator />
     </main>
   )
--- src/components/Amount.tsx
+++ src/components/Amount.tsx
@@ -8,13 +8,21 @@ const Amount: React.FC<AmountProps> = ({ tipAmount, total, handleReset }) => {
   return (
     <div className="amount">
       <div className="amount__info">
-        <div className="amount__tip">
-          <p> Tip Amount / person</p>
-          <span>{tipAmount}</span>
+        <div className="amount__val">
+          <div>
+            <p> Tip Amount</p>
+            <p className="amount__person">/ person</p>
+          </div>
+          <span className="price">${tipAmount.toFixed(2)}</span>
         </div>
-        <div className="amount__total">
-          <p>Total / person</p>
-          <span>{total}</span>
+        <div className="amount__val">
+          <div>
+            <p>Total</p>
+            <p className="amount__person">/ person</p>
+          </div>
+          <span className="price">
+            ${Number.isNaN(total) ? '0.00' : total.toFixed(2)}
+          </span>
         </div>
       </div>
       <button className="amount__btn" onClick={handleReset}>
