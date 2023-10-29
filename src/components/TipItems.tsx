2f13f0d7b2533983981c22d9df66e7b455e7b31d
Prashanth Balakrishnan <prashbala13@gmail.com>
2023-10-29 17:12:11 -0400

    added styles to everything but amount component

--- src/components/TipItems.tsx
+++ src/components/TipItems.tsx
@@ -1,12 +1,24 @@
+import clsx from 'clsx'
+
 interface TipItemsProps {
   percentage: number
   setTip: React.Dispatch<React.SetStateAction<number>>
+  label: string
+  tip: number
 }
 
-const TipItems: React.FC<TipItemsProps> = ({ percentage, setTip }) => {
+const TipItems: React.FC<TipItemsProps> = ({
+  percentage,
+  setTip,
+  label,
+  tip,
+}) => {
   return (
-    <button onClick={() => setTip(percentage)}>
-      <span>{percentage}%</span>
+    <button
+      className={clsx('tip__btn', tip === percentage ? 'active__btn' : null)}
+      onClick={() => setTip(percentage)}
+    >
+      <span>{label}</span>
     </button>
   )
 }
--- src/data.ts
+++ src/data.ts
@@ -2,22 +2,27 @@ const data = [
   {
     id: 1,
     tipPercentage: 0.05,
+    label: '5%',
   },
   {
     id: 2,
     tipPercentage: 0.1,
+    label: '10%',
   },
   {
     id: 3,
     tipPercentage: 0.15,
+    label: '15%',
   },
   {
     id: 4,
     tipPercentage: 0.25,
+    label: '25%',
   },
   {
     id: 5,
     tipPercentage: 0.5,
+    label: '50%',
   },
 ]
 
